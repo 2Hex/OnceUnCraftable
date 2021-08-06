57ecb47a99f5a3a569de23db9febb1fe684e3555
2Hex <rxgamerhere@gmail.com>
2021-08-06 18:43:41 +0200

    Second Commit

--- src/main/java/me/hex/onceuncraftable/Onceuncraftable.java
+++ src/main/java/me/hex/onceuncraftable/Onceuncraftable.java
@@ -2,7 +2,6 @@ package me.hex.onceuncraftable;
 
 import me.hex.onceuncraftable.commands.Changeconfig;
 import me.hex.onceuncraftable.events.Mineable;
-import me.hex.onceuncraftable.events.onJoin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Onceuncraftable extends JavaPlugin  {
@@ -12,8 +11,6 @@ public final class Onceuncraftable extends JavaPlugin  {
     public void onEnable() {
         INSTANCE = this;
 
-        recipes.addRecipes();
-        getServer().getPluginManager().registerEvents(new onJoin(recipes), this);
         getServer().getPluginManager().registerEvents(new Mineable(), this);
         getCommand("mineablespawners").setExecutor(new Changeconfig());
         getCommand("craftenchants").setExecutor(new Changeconfig());
--- src/main/java/me/hex/onceuncraftable/commands/Changeconfig.java
+++ src/main/java/me/hex/onceuncraftable/commands/Changeconfig.java
@@ -13,87 +13,87 @@ public class Changeconfig implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (command.getName().equals("mineablespawners")) {
 
-            if (sender.hasPermission("once.mineable")) {
-                if (args.length > 0) {
-
-                    if (args[0].equals("true")) {
-                        if (Onceuncraftable.INSTANCE.getConfig().getBoolean("mine-spawners-with-silk-touch")) {
-                            sender.sendMessage(ChatColor.GREEN + "It's already true!");
-                        } else {
-                            Onceuncraftable.INSTANCE.getConfig().set("mine-spawners-with-silk-touch", true);
-                            sender.sendMessage(ChatColor.GREEN + "Mineable spawners is turned on!");
-                            Onceuncraftable.INSTANCE.saveConfig();
-                        }
-                    }
-                    if (args[0].equals("false")) {
-                        if (!Onceuncraftable.INSTANCE.getConfig().getBoolean("mine-spawners-with-silk-touch")) {
-                            sender.sendMessage(ChatColor.GREEN + "It's already false!");
-                        } else {
-                            Onceuncraftable.INSTANCE.getConfig().set("mine-spawners-with-silk-touch", false);
-                            sender.sendMessage(ChatColor.GREEN + "Mineable spawners is turned off!");
-                            Onceuncraftable.INSTANCE.saveConfig();
-
-                        }
-                    }
-                    if (!(args[0].equalsIgnoreCase("true") || !(args[0].equalsIgnoreCase("false")))) {
-
-                        sender.sendMessage("You can only set mineable spawners to 'true' or 'false'!");
-                    }
-
-                } else {
-                    sender.sendMessage("Use /minespawners true/false!");
-                }
-            } else {
+            if (!sender.hasPermission("once.mineable")) {
                 sender.sendMessage("You don't have access to this command!. you need the permission 'once.mineable'.");
+                return false;
             }
+            if (args.length > 0) {
 
+                if (args[0].equals("true")) {
+                    Onceuncraftable.INSTANCE.getConfig().set("mine-spawners-with-silk-touch", true);
+                    sender.sendMessage(ChatColor.GREEN + "Mineable spawners is turned on!");
+                    Onceuncraftable.INSTANCE.saveConfig();
 
+                }
+                if (Onceuncraftable.INSTANCE.getConfig().getBoolean("mine-spawners-with-silk-touch")) {
+                    sender.sendMessage(ChatColor.GREEN + "It's already true!");
 
+                }
 
+                if (args[0].equals("false")) {
 
+                    Onceuncraftable.INSTANCE.getConfig().set("mine-spawners-with-silk-touch", false);
+                    sender.sendMessage(ChatColor.GREEN + "Mineable spawners is turned off!");
+                    Onceuncraftable.INSTANCE.saveConfig();
+
+                }
+                if (!Onceuncraftable.INSTANCE.getConfig().getBoolean("mine-spawners-with-silk-touch")) {
+                    sender.sendMessage(ChatColor.GREEN + "It's already false!");
+
+                }
+                if (!(args[0].equalsIgnoreCase("true") || !(args[0].equalsIgnoreCase("false")))) {
+
+                    sender.sendMessage("You can only set mineable spawners to 'true' or 'false'!");
+
+                }
+                return true;
+            }
         }
         if (command.getName().equals("craftenchants")) {
-            if (sender.hasPermission("once.craftenchants")) {
-                if (args.length > 0) {
-                    if (args[0].equals("true")) {
-                        if (Onceuncraftable.INSTANCE.getConfig().getBoolean("craft-enchants")) {
-                            sender.sendMessage(ChatColor.GREEN + "It's Already true!");
-                        } else {
-                            Onceuncraftable.INSTANCE.getConfig().set("craft-enchants", true);
-                            sender.sendMessage(ChatColor.GREEN + "Craft enchanted is turned on!");
-                            Onceuncraftable.INSTANCE.saveConfig();
-
-                        }
-                    }
-
-                    if (args[0].equals("false")) {
-                        if (!Onceuncraftable.INSTANCE.getConfig().getBoolean("craft-enchants")) {
-                            sender.sendMessage(ChatColor.GREEN + "It's Already false!");
-                        } else {
-                            Onceuncraftable.INSTANCE.getConfig().set("craft-enchants", false);
-                            sender.sendMessage(ChatColor.GREEN + "Craft enchanted is turned off!");
-                            Onceuncraftable.INSTANCE.saveConfig();
-
-                        }
-                    }
-                    if (!(args[0].equalsIgnoreCase("true") || !(args[0].equalsIgnoreCase("false")))) {
-
-                        sender.sendMessage("You can only set craftable enchants to 'true' or 'false'!");
-                    }
-
-                } else {
-                    sender.sendMessage("Use /craftenchants true/false");
-                }
-            }  else {
+            if (!sender.hasPermission("once.craftenchants")) {
                 sender.sendMessage("You don't have access to this command!. you need the permission 'once.craftenchants'.");
-        }
+                return true;
+            }
+            if (args.length > 0) {
+                if (args[0].equals("true")) {
+                    if (!Onceuncraftable.INSTANCE.getConfig().getBoolean("craft-enchants")) {
+                        Onceuncraftable.INSTANCE.getConfig().set("craft-enchants", true);
+                        sender.sendMessage(ChatColor.GREEN + "Craft enchanted is turned on!");
+                        Onceuncraftable.INSTANCE.saveConfig();
+                        return true;
+                    }
+                    sender.sendMessage(ChatColor.GREEN + "It's Already true!");
+
+
+                }
+
+
+                if (args[0].equals("false")) {
+                    if (Onceuncraftable.INSTANCE.getConfig().getBoolean("craft-enchants")) {
+                        Onceuncraftable.INSTANCE.getConfig().set("craft-enchants", false);
+                        sender.sendMessage(ChatColor.GREEN + "Craft enchanted is turned off!");
+                        Onceuncraftable.INSTANCE.saveConfig();
+                        return true;
+                    }
+                    sender.sendMessage(ChatColor.GREEN + "It's Already false!");
+
+                }
+
+                if (!(args[0].equalsIgnoreCase("true") || !(args[0].equalsIgnoreCase("false")))) {
+
+                    sender.sendMessage("You can only set craftable enchants to 'true' or 'false'!");
+                }
+            }
 
             return true;
         }
-    return true;
+        return true;
     }
-
 }
+
+
+
+
 
 
 

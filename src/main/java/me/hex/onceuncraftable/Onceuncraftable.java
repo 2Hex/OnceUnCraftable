dc3f396520c0bb287f807809d249d24d0de71419
2Hex <rxgamerhere@gmail.com>
2021-08-10 21:16:29 +0200

    Official new Update (Many new features)

--- src/main/java/me/hex/onceuncraftable/Onceuncraftable.java
+++ src/main/java/me/hex/onceuncraftable/Onceuncraftable.java
@@ -2,35 +2,51 @@ package me.hex.onceuncraftable;
 
 import me.hex.onceuncraftable.commands.ChangeConfig;
 import me.hex.onceuncraftable.events.Mineable;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.IOException;
+
 public final class Onceuncraftable extends JavaPlugin {
+    private File customConfigFile;
+    private FileConfiguration customConfig;
     @Override
     public void onEnable() {
 
         getServer().getPluginManager().registerEvents(new Mineable(this), this);
         getCommand("mineablespawners").setExecutor(new ChangeConfig(this));
         getCommand("craftenchants").setExecutor(new ChangeConfig(this));
-        saveDefaultConfig();
-        getServer().getPluginManager().registerEvents(new RecipesManager(this), this);
         getCommand("onceuncraftable").setExecutor(new ChangeConfig(this));
-        if(getConfig().getString("chance-to-spawn-spawner-eggs") == null){
-            getConfig().createSection("chance-to-spawn-spawner-eggs");
-            getConfig().set("chance-to-spawn-spawner-eggs", 100);
-        }
-        if(getConfig().getString("craft-enchants") == null) {
-            getConfig().createSection("craft-enchants");
-            getConfig().set("craft-enchants", true);
-        }
-        if(getConfig().getString("disabled_recipes") == null){
-            getConfig().createSection("disabled_recipes:");
-            for(int i = 0; i < 5; i++) {
-                getConfig().set("disabled_recipes:.-", " ");
-            }
-        }
-            saveConfig();
+        saveDefaultConfig();
+        createCustomConfig();
+        getServer().getPluginManager().registerEvents(new RecipesManager(this), this);
+
+
     }
-}
+
+    public FileConfiguration getCustomConfig() {
+        return this.customConfig;
+    }
+
+    private void createCustomConfig() {
+        customConfigFile = new File(getDataFolder(), "recipes.yml");
+        if (!customConfigFile.exists()) {
+            customConfigFile.getParentFile().mkdirs();
+            saveResource("recipes.yml", false);
+        }
+
+        customConfig = new YamlConfiguration();
+        try {
+            customConfig.load(customConfigFile);
+        } catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    }
+
 
 
